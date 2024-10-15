792a1706f005bbee0330681cdb44fc9f02daa5e0
christiangit <colvera1218@gmail.com>
2024-10-15 13:36:05 -0500

    Added nav css and working on MovieList css

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,22 +1,22 @@
 
 import { Link } from "react-router-dom";
-
-export default function MovieList({movies}){
-
+import styles from './MovieList.module.css'; // Import the CSS module
+export default function MovieList({ movies }) {
     const movieList = movies.map((movie) => {
-        return(
-            <Link key={movie._id} to={`/movies/${movie._id}`}>
+        return (
+            <Link key={movie._id} to={`/movies/${movie._id}`} className={styles.movieItem}>
                 <article>
                     <header>
                         <h2>{movie.title}</h2>
                     </header>
-                    <img src={movie.image} alt={movie.title}/>
+                    <img src={movie.image} alt={movie.title} />
                 </article>
             </Link>
-        )
-    })
-
-    return(
-        <main>{movieList}</main>
-    )
+        );
+    });
+    return (
+        <main className={styles.movieList}>
+            {movieList}
+        </main>
+    );
 }
